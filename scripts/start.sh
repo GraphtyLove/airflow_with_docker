5f7834be44e7d07106a7155f5f906e997372a98a
GraphtyLove <maxim.berge@gmail.com>
2022-11-22 16:14:10 +0100

    Update start.sh to add init script

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,5 +4,7 @@
 docker build -f dags/immo_eliza_pipeline/scraper/Dockerfile -t airflow_scraper:latest ./dags/immo_eliza_pipeline/scraper;
 docker build -f dags/immo_eliza_pipeline/data_cleaner/Dockerfile -t airflow_data_cleaner:latest ./dags/immo_eliza_pipeline/data_cleaner;
 
+# Init Airflow
+docker compose up airflow-init;
 # Run Airflow
-docker-compose up -d;
+docker compose up -d;
